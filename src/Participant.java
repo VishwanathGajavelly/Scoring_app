d66be1bc26dd5581e413935cb0960631eccf63a9
vishwa.gajavelly <vishwa.gajavelly@CA-L1LPHSQ2.groupinfra.com>
2020-02-06 22:08:57 -0500

    initial commit

---
+++ src/Participant.java
@@ -0,0 +1,34 @@
+
+public class Participant {
+	
+	private String firstName;
+	private int chestNumber;
+	private int age;
+	private String state;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public int getChestNumber() {
+		return chestNumber;
+	}
+	public void setChestNumber(int chestNumber) {
+		this.chestNumber = chestNumber;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+
+}
